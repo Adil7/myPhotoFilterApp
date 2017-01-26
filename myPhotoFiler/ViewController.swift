56c6586fad68ee0f32bf65d15d1f6a1ab98e5de4
Adil7 <Adil@adils-mbp.lan>
2017-01-26 02:26:09 -0500

    Initial Commit

---
+++ myPhotoFiler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myPhotoFiler
+//
+//  Created by Adil Imran on 2017-01-26.
+//  Copyright © 2017 AIM Co.Adil Imran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
